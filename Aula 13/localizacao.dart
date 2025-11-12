fe6d89cc205b71fce7940861f2882f189a85039a
Dav1i <b.davi@aluno.ifsp.edu.br>
2025-11-11 21:20:29 -0300

    Update localizacao.dart

--- Aula 13/localizacao.dart
+++ Aula 13/localizacao.dart
@@ -4,7 +4,6 @@ class Localizacao {
   double? latitude;
   double? longitude;
 
-  // Método que pega a localização atual
   Future<void> pegaLocalizacaoAtual() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
@@ -12,3 +11,4 @@ class Localizacao {
     longitude = position.longitude;
   }
 }
+
